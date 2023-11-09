fff3c00ea3f590441493e40c6f5f80998473d14a
Alex Ionas <104205902+StormS31@users.noreply.github.com>
2023-11-09 18:11:47 +0200

    deleted unused directory

--- Frontend/frontend/src/enviroments/enviroment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const enviroment = {
-    production: true,
-    backendUsers: 'http://backend-users:8080/users',
-    backendDevices: 'http://backend-devices:8081/devices',
-    backendDevicesUsers: 'http://backend-devices:8081/users'
-}
--- Frontend/frontend/src/enviroments/enviroment.ts
+++
@@ -1,6 +0,0 @@
-export const enviroment = {
-    production: false,
-    backendUsers: 'http://localhost:8080',
-    backendDevices: 'http://localhost:8081',
-    backendDevicesUsers: 'http://localhost:8081/users'
-}
